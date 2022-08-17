5c1b2f582e68c9d4d5416aaee8b0f648e09a9561
Lykcord <cxrlosroleplay@gmail.com>
2022-08-18 00:30:11 +0200

    MenuTitle WIP

--- src/main/kotlin/de/coaster/cringepvp/listeners/MenuListener.kt
+++ src/main/kotlin/de/coaster/cringepvp/listeners/MenuListener.kt
@@ -1,7 +1,10 @@
 package de.coaster.cringepvp.listeners
 
 import de.coaster.cringepvp.extensions.plainText
+import de.coaster.cringepvp.extensions.toCringeUser
+import de.moltenKt.paper.extension.display.ui.itemStack
 import de.moltenKt.unfold.text
+import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
@@ -14,16 +17,40 @@ class MenuListener : Listener {
     fun onInventoryClick(event: InventoryClickEvent) = with(event) {
         val titleComponent = view.title()
         val title = titleComponent.plainText
-        if (title != "Dein Menü") return@with
-        isCancelled = true
         if (currentItem == null) return@with
-        if (currentItem!!.type != Material.CHEST) return@with
+        when(title){
+            "Dein Menü" -> onMenuClick(this)
+            "Titel" -> onTitelMenuClick(this)
+        }
+
+    }
+
+    fun onTitelMenuClick(event: InventoryClickEvent) = with(event){
+        isCancelled = true
+    }
+
+    fun onMenuClick(event: InventoryClickEvent) = with(event){
+        isCancelled = true
         val player = whoClicked as Player
 
         when(slot) {
-            11 -> player.sendMessage(text("Deine Titel"))
+            11 -> {
+                val titleMenu = Bukkit.createInventory(null, 36, text("Titel"))
+
+                val cringeUser = player.toCringeUser()
+                val titles = cringeUser.ownedTitles
+
+                titles.forEachIndexed { index, title ->
+
+
+                    titleMenu.setItem(
+                        index,
+                        Material.NAME_TAG.itemStack { editMeta { meta -> meta.displayName(text(title.name)) } })
+                }
+                player.openInventory(titleMenu)
+            }
             15 -> player.sendMessage(text("Deine Skills"))
+            else -> {}
         }
     }
-
 }
